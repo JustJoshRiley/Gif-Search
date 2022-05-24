70b161ed6ff83ef7963073006ec88c2ab92373a1
justjoshriley <joshriley316@gmail.com>
2022-05-23 20:05:05 -0700

    added search form

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
+    console.log(req.query) // => "{ term: hey" }[/bold]
+    res.render('home')
+})
+
+app.get('/', (req, res) => {
     const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
     res.render('hello-gif', {gifUrl});
 });
